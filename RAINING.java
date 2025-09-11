b8ea00e0af2b43d51e83b3310c0fab30dd912198
bhavi17dsu <allaparthibhavitha@gmail.com>
2025-09-11 12:14:38 +0530

    Create RAINING.java

---
+++ RAINING.java
@@ -0,0 +1,10 @@
+public class LogicalnotExample{
+    public static void main(String[] args){
+        boolean isRaining=false;
+        if(!isRaining){
+            System.out.println("you can go outside without umbrella");
+        }else{
+            System.out.println("take an umbrella");
+        }
+    }
+}
