8f77b85afaf9a4462c6f51dba8f052291167c83a
bhavi17dsu <allaparthibhavitha@gmail.com>
2025-11-16 15:58:06 +0530

    Create assignmentoperators.java

---
+++ assignmentoperators.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+        num += 5;   // num = num + 5
+        System.out.println("After += operation: " + num);
+        num -= 3;   // num = num - 3
+        System.out.println("After -= operation: " + num);
+        num *= 4;   // num = num * 4
+        System.out.println("After *= operation: " + num);
+    }
+}
